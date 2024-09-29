8f09a361ed63b79d91f6c36a2ba0579c1cbc1608
Oscar Valois <118651508+osvalois@users.noreply.github.com>
2024-09-29 09:56:11 -0600

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,59 @@ import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    astroClientConfig(),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
     },
+    plugins: [
+      {
+        name: 'client-runtime-config',
+        transformIndexHtml() {
+          return [
+            {
+              tag: 'script',
+              attrs: { type: 'module' },
+              children: `import * as client from 'astro:client';`,
+            },
+          ];
+        },
+      },
+    ],
   },
+  output: 'server',
 });
+
+function astroClientConfig() {
+  return {
+    name: 'astro-client-config',
+    hooks: {
+      'astro:config:setup': ({ updateConfig }) => {
+        updateConfig({
+          vite: {
+            plugins: [
+              {
+                name: 'client-runtime-config',
+                transformIndexHtml() {
+                  return [
+                    {
+                      tag: 'script',
+                      attrs: { type: 'module' },
+                      children: `import * as client from 'astro:client';`,
+                    },
+                  ];
+                },
+              },
+            ],
+          },
+        });
+      },
+    },
+  };
+}
